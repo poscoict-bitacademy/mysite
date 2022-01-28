302d2f4d141a15e5954d00be20b552d53a20aa43
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-01-28 16:56:36 +0900

    톰캣 플러그인 설정

--- mysite03/src/main/java/com/poscoict/mysite/repository/BoardRepository.java
+++ mysite03/src/main/java/com/poscoict/mysite/repository/BoardRepository.java
@@ -1,16 +1,13 @@
 package com.poscoict.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
 import com.poscoict.mysite.vo.BoardVo;
 
 @Repository
