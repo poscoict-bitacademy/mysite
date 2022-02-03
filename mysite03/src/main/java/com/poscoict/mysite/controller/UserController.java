6fd19b28d6c6dbc89717ac6f9329bed035cd0d1a
poscoict-bitacademy <poscoict.bitacademy@gmail.com>
2022-02-03 10:47:07 +0900

    validation #2

--- mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.poscoict.mysite.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -29,18 +27,19 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(@Valid UserVo userVo, BindingResult result, Model model) {
+	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model) {
 		if(result.hasErrors()) {
 //			List<ObjectError> list = result.getAllErrors();
 //			for(ObjectError error : list) {
 //				System.out.println(error);
 //			}
 			
+			// model.addAttribute("userVo",  vo);
 			model.addAllAttributes(result.getModel());
 			return "user/join";
 		}
 		
-		userService.join(userVo);
+		userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
 	
--- mysite03/src/main/webapp/WEB-INF/views/user/join.jsp
+++ mysite03/src/main/webapp/WEB-INF/views/user/join.jsp
@@ -15,15 +15,16 @@
 		<c:import url="/WEB-INF/views/includes/header.jsp"/>
 		<div id="content">
 			<div id="user">
-
 				<form id="join-form" name="joinForm" method="post" action="${pageContext.request.contextPath }/user/join">
 					<label class="block-label" for="name">이름</label>
-					<input id="name" name="name" type="text" value="">
-					<spring:hasBindErrors name="userVo">
-						<c:if test="${}">
-						</c:if>
-					</spring:hasBindErrors>
-					
+					<input id="name" name="name" type="text" value="${userVo.name }">
+					<p style="text-align:left; padding-left:0; color:#f00">
+						<spring:hasBindErrors name="userVo">
+							<c:if test="${errors.hasFieldErrors('name') }">
+								<spring:message code="${errors.getFieldError('name').codes[0] }" />
+							</c:if>
+						</spring:hasBindErrors>
+					</p>
 					<label class="block-label" for="email">이메일</label>
 					<input id="email" name="email" type="text" value="">
 					<input type="button" value="id 중복체크">
