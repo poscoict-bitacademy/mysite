36b93e8c9384ae076acca2823e07b12e0c8ca939
포스코ICT-비트아카데미 <97428243+poscoict-bitacademy@users.noreply.github.com>
2022-03-05 11:25:30 +0900

    Update GuestbookController.java

--- mysite03/src/main/java/com/poscoict/mysite/controller/api/GuestbookController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/api/GuestbookController.java
@@ -1,10 +1,36 @@
-package com.poscoict.mysite.controller.api;
+import com.poscoict.mysite.dto.JsonResult;
+import com.poscoict.mysite.vo.GuestbookVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.poscoict.mysite.service.GuestbookService;
 
-@RestController("/guestbookApiController")
+import java.util.List;
+
+@RestController("guestbookApiController")
 @RequestMapping("/guestbook/api")
 public class GuestbookController {
+	@Autowired
+	private GuestbookService guestbookService;
 
+	@GetMapping("/list/{no}")
+	public JsonResult list(@PathVariable("no") Long startNo) {
+		List<GuestbookVo> list = guestbookService.getMessageList(startNo);
+		return JsonResult.success(list);
+	}
+
+	@PostMapping("/add")
+	public JsonResult add(@RequestBody GuestbookVo vo) {
+		guestbookService.addMessage(vo);
+		vo.setPassword("");
+		return JsonResult.success(vo);
+	}
+
+	@DeleteMapping("/delete/{no}")
+	public JsonResult delete(
+			@PathVariable("no") Long no,
+			@RequestParam(value="password", required=true, defaultValue="") String password) {
+		boolean result = guestbookService.deleteMessage(no, password);
+		return JsonResult.success(result ? no : -1);
+	}
 }
